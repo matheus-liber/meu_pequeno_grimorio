6c7dedeec2b92d5f5350cffe0012913156abd61e
matheus-liber <matheuslibergt@gmail.com>
2024-04-02 14:22:39 -0300

    Configuring carousel on Dashboard

---
+++ lib/screens/components/carousel_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CarouselItem extends StatelessWidget {
+  final String imagePath;
+  final String text;
+
+  const CarouselItem({super.key, required this.imagePath, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset('assets/carousel/$imagePath'),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+            fontWeight: FontWeight.w500,
+            fontFamily: "Bigelow Rules"
+          ),
+        )
+      ],
+    );
+  }
+}
